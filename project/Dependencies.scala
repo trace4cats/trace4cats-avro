f69a9df0525f639bbec29cec83e019cb6ebccc78
catostrophe <40268503+catostrophe@users.noreply.github.com>
2021-08-03 19:12:32 +0300

    Add tests for the manual codecs

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,6 +12,7 @@ object Dependencies {
     val log4cats = "2.1.1"
     val logback = "1.2.5"
     val vulcan = "1.7.1"
+    val slf4j = "1.7.32"
 
     val kindProjector = "0.13.0"
     val betterMonadicFor = "0.3.1"
@@ -21,10 +22,12 @@ object Dependencies {
   lazy val trace4catsModel = "io.janstenpickle"          %% "trace4cats-model"           % Versions.trace4cats
   lazy val trace4catsTestkit = "io.janstenpickle"        %% "trace4cats-testkit"         % Versions.trace4cats
 
-  lazy val fs2Io = "co.fs2"           %% "fs2-io"          % Versions.fs2
-  lazy val log4cats = "org.typelevel" %% "log4cats-slf4j"  % Versions.log4cats
-  lazy val logback = "ch.qos.logback"  % "logback-classic" % Versions.logback
-  lazy val vulcan = "com.github.fd4s" %% "vulcan"          % Versions.vulcan
+  lazy val fs2Io = "co.fs2"                  %% "fs2-io"          % Versions.fs2
+  lazy val log4cats = "org.typelevel"        %% "log4cats-slf4j"  % Versions.log4cats
+  lazy val logback = "ch.qos.logback"         % "logback-classic" % Versions.logback
+  lazy val vulcan = "com.github.fd4s"        %% "vulcan"          % Versions.vulcan
+  lazy val vulcanGeneric = "com.github.fd4s" %% "vulcan-generic"  % Versions.vulcan
+  lazy val slf4jNop = "org.slf4j"             % "slf4j-nop"       % Versions.slf4j
 
   lazy val kindProjector = ("org.typelevel" % "kind-projector"     % Versions.kindProjector).cross(CrossVersion.full)
   lazy val betterMonadicFor = "com.olegpy" %% "better-monadic-for" % Versions.betterMonadicFor
